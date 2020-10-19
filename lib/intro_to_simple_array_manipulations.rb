d9484381acfbedbedfd82c2baf64661e4157c55d
Nhon Dang <dang.nhon12345@yahoo.com>
2020-10-19 21:36:56 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,11 +1,11 @@
-def using_push(Array, String)
-  Array.push(String)
+def using_push(array, string)
+  array.push(string)
 end
 
-def using_unshift(Array, String)
-  Array.unshift(String)
+def using_unshift(array, string)
+  array.unshift(string)
 end
 
-def using_unshift(Array, String)
-  Array.pop(String)
+def using_unshift(array, string)
+  array.pop(string)
 end
